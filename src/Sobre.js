f77e76cde445499ce7c8ff57e302a34d1100e1d2
Andre Bom Alves Nunes <andrebom@MacBook-Air-de-Andre.local>
2019-10-10 13:20:50 -0300

    ajuste do Sobre

--- src/Sobre.js
+++ src/Sobre.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Sobre() {
     return (
-        <div className='container'><h1>Sobre</h1></div>
+        <div className='container'><h1>Sobre</h1> <p>Desenvolvido por Andre Bom</p></div>
     )
 }
 
